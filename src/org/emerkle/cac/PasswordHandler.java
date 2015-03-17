305b28507b2843d8ebb91cfacdb45e994e3eb8ca
Erik Merkle <github@emerkle.net>
2015-03-16 21:08:30 -0500

    initial push

---
+++ src/org/emerkle/cac/PasswordHandler.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.emerkle.cac;
+
+/**
+ *
+ * @author emerkle
+ */
+interface PasswordHandler {
+    void handlerPassword(char[] password);
+}
